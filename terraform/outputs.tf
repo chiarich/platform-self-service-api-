97454ff3db9292f2c2f6c073df63b58cee3e20c0
chiarich <64822270+chiarich@users.noreply.github.com>
2026-04-05 19:46:14 -0400

    Publish exact API endpoints and deployment guide

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "api_url" {
+  description = "Base API Gateway URL"
+  value       = aws_apigatewayv2_api.api.api_endpoint
+}
+
+output "create_bucket_url" {
+  description = "Endpoint for creating S3 buckets"
+  value       = "${aws_apigatewayv2_api.api.api_endpoint}/buckets"
+}
+
+output "buckets_url" {
+  description = "Endpoint for listing S3 bucket requests"
+  value       = "${aws_apigatewayv2_api.api.api_endpoint}/buckets"
+}
+
+output "lambda_function_name" {
+  description = "Deployed Lambda function name"
+  value       = aws_lambda_function.api.function_name
+}
+
+output "sns_topic_arn" {
+  description = "SNS topic ARN for alerts"
+  value       = aws_sns_topic.alerts.arn
+}
